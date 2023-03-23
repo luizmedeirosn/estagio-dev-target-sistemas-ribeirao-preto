b4f0d9d390bcbdd7b58671a567453831a5321395
Neto Medeiros <netomedeiros1500@gmail.com>
2023-03-22 21:57:23 -0300

    Refactored the program

--- QuestionTwoFibonacci.java
+++ QuestionTwoFibonacci.java
@@ -1,33 +1,36 @@
 import java.util.*;
 
 public class QuestionTwoFibonacci {
-    public static void main(String [] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
 
         Locale.setDefault(Locale.US);
-        
+
         Scanner sc = new Scanner(System.in);
-        try {
-            Integer num = sc.nextInt();
-            Boolean belongs = belongsSequentialFibonacci(num);
-            if (belongs) {
-                System.out.println("Belongs...");
-            } else {
-                System.out.println("Not Belong...");
+        Integer num = 1;
+        while (num >= 0) {
+            try {
+                num = sc.nextInt();
+                Boolean belongs = belongsSequentialFibonacci(num);
+                if (belongs) {
+                    System.out.println("Belongs...");
+                } else {
+                    System.out.println("Not Belong...");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Informe um inteiro válido!");
+                sc.next();
             }
-        } catch(InputMismatchException e) {
-            System.out.println("Informe um inteiro válido!");
-        } finally {
-            sc.close();
         }
+        sc.close();
     }
 
     // O design do método foi feita tendo em vista a implementação de um cenário de testes.
     private static Integer a = 0, b = 1;
     private static Boolean belongsSequentialFibonacci(Integer num) {
-        if (num.intValue() == a.intValue() ) {
+        if (num.intValue() == a.intValue()) {
             return true;
-        } else if (num.intValue()  < a.intValue() ) {
+        } else if (num.intValue() < a.intValue()) {
             return false;
         } else {
             int aux = a;
